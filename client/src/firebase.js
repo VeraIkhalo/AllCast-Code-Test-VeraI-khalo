cb702d5631b97549f386e953d4890f6d0f16e715
VeraIkhalo1 <vera@worktechlabs.io>
2024-05-21 20:44:10 +0100

    Fix video upload error

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: "fir-4f21e.firebaseapp.com",
-  projectId: "fir-4f21e",
-  storageBucket: "fir-4f21e.appspot.com",
-  messagingSenderId: "339782082505",
-  appId: "1:339782082505:web:15c040e28589fafe0bd218",
-  measurementId: "G-VWNEDPY3V9"
+  apiKey: 'AIzaSyCypWmw-g8wakW4nzUjepAOpGwTMxX1pIE',
+  authDomain: "video-sharing-a3520.firebaseapp.com",
+  projectId: "video-sharing-a3520",
+  storageBucket: "video-sharing-a3520.appspot.com",
+  messagingSenderId: "316850995340",
+  appId: "1:316850995340:web:7e7693b1257ec469aa3ba9",
+  measurementId: "G-F75XWQGX9Z"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 
 export default app;
